bd3b63e0b7c6290d5def58ca6faf8ef81a36b5b5
miruthula <miruthula183@gmail.com>
2026-07-09 01:21:57 -0700

    Add sumofnumbers program

---
+++ sumofnumbers.java
@@ -0,0 +1,12 @@
+public class sumofnumbers {
+    public static void main(String[] args){
+        int i=0;
+        int sum=0;
+        while(i<=10){
+            sum=sum+i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+    
+}
