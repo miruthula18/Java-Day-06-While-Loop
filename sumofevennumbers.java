47827c53785d9b7c6bd1db83b8f7ccfbca99cb13
miruthula <miruthula183@gmail.com>
2026-07-09 01:26:47 -0700

    Add sumofevennumbers.java

---
+++ sumofevennumbers.java
@@ -0,0 +1,12 @@
+public class sumofevennumbers {
+    public static void main(String[] args){
+        int i=2;
+        int sum=0;
+        while(i<=10){
+            sum=sum+i;
+            i=i+2;
+        }
+        System.out.print(sum);
+    }
+    
+}
