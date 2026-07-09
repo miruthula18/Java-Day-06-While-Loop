1495b16b399537a8569e0a70dac0ba9e4796e4f3
miruthula <miruthula183@gmail.com>
2026-07-09 01:23:53 -0700

    Add reversenumber.java

---
+++ reversenumber.java
@@ -0,0 +1,10 @@
+public class reversenumber {
+    public static void main(String[] args){
+        int i=10;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+    
+}
