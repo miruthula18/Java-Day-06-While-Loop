63cfded793c2fe7d7825c5dbbabae56399e8c129
miruthula <miruthula183@gmail.com>
2026-07-09 01:24:22 -0700

    Add countdown.java

---
+++ countdown.java
@@ -0,0 +1,12 @@
+public class countdown {
+    public static void main(String[] args){
+        int i=5;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+        System.out.print("go");
+    }
+    
+}
+
