c46dc95d6c0fa613e889e3c6c633cda36c219dc0
miruthula <miruthula183@gmail.com>
2026-07-09 01:23:22 -0700

    Add multiplicationtable.java

---
+++ multiplicationtable.java
@@ -0,0 +1,10 @@
+public class multiplicationtable {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            System.out.println("18x"+i+"="+(18*i));
+            i++;
+        }
+    }
+    
+}
